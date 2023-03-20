72bb7dc57bede91668e3bbd8f2b795ba02ec2d75
hydee001 <127431499+hydee001@users.noreply.github.com>
2023-03-20 17:42:42 +0100

    Create week2.php

---
+++ week2.php
@@ -0,0 +1,18 @@
+<?php
+$voters = do you have "PVC";
+if($voters ="PVC"){
+    echo"voter eligible to vote";
+}
+$voters_age = 18;
+
+if($voters_age<= 18){
+ echo "voter is not up to the age to vote";
+}elseif ($voters_age>= 18) {
+    echo"voter is eligible to vote"
+}
+$voter_ward = 020;
+if($voter_ward == 020){
+    echo"voter is eligible to vote" 
+}
+
+?>
